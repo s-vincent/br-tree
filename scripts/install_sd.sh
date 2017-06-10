bee0972817cd3e1373c4d1149d93d3d36d02e3b9
Sebastien Vincent <sebastien.vincent@cppextrem.com>
2017-06-10 08:38:21 +0200

    Fixes xenomai configuration (no serial input).

--- scripts/install_sd.sh
+++ scripts/install_sd.sh
@@ -2,7 +2,6 @@
   
 sudo tar xf output/images/rootfs.tar -C /media/seb/ROOT/
 cp output/images/bcm2837-rpi-3-b.dtb /media/seb/BOOT/
-cp output/images/bcm2710-rpi-3-b.dtb /media/seb/BOOT/
 cp output/images/Image /media/seb/BOOT/
 cp output/images/zImage /media/seb/BOOT/
 cp output/images/u-boot.bin /media/seb/BOOT/
