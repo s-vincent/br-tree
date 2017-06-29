6ba306df76e9e929000209a723b4fa8b08879ed2
Sebastien Vincent <sebastien.vincent@cppextrem.com>
2017-06-29 19:58:28 +0200

    Uses "whoami" to find user for the install_sd.sh script.

--- scripts/install_sd.sh
+++ scripts/install_sd.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-  
-sudo tar xf output/images/rootfs.tar -C /media/seb/ROOT/
-cp output/images/bcm2837-rpi-3-b.dtb /media/seb/BOOT/
-cp output/images/Image /media/seb/BOOT/
-cp output/images/zImage /media/seb/BOOT/
-cp output/images/u-boot.bin /media/seb/BOOT/
-cp output/images/boot.scr /media/seb/BOOT/
-cp -r output/images/rpi-firmware/* /media/seb/BOOT/
+
+$user=$(whoami)
+
+sudo tar xf output/images/rootfs.tar -C /media/$user/ROOT/
+cp output/images/bcm2837-rpi-3-b.dtb /media/$user/BOOT/
+cp output/images/Image /media/$user/BOOT/
+cp output/images/zImage /media/$user/BOOT/
+cp output/images/u-boot.bin /media/$user/BOOT/
+cp output/images/boot.scr /media/$user/BOOT/
+cp -r output/images/rpi-firmware/* /media/$user/BOOT/
+
+sync
+umount /media/$user/BOOT
+umount /media/$user/ROOT
 
